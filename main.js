78482417ff23a7a3149fb5093604c5959cd1f119
ManuelHMR <manuelhmrjf@gmail.com>
2022-02-02 21:49:07 -0300

    Gerar cartas

---
+++ main.js
@@ -0,0 +1,21 @@
+let pairNum = 0
+const main = document.querySelector('main')
+function displayCards() {
+  main.innerHTML = ``
+  for (let i = 0; i < pairNum*2; i++) {
+    main.innerHTML = main.innerHTML + `<div class="card">
+    <img class="frontImg" src="media/front.png" alt="papagaio verde fofinho"/>
+    </div>`
+  }
+}
+function gameSize() {
+  pairNum = prompt(
+    'Com quantos pares de cartas deseja jogar? (limite 2-7 pares)'
+  )
+  while (pairNum > 7 || pairNum < 2) {
+    pairNum = parseInt(
+      prompt('Com quantos pares de cartas deseja jogar? (limite 2-7 pares)')
+    )
+  }
+  displayCards()
+}
