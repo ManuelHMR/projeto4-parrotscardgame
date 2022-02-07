773a2d8f38c682973c7c098158204a0d4305046f
ManuelHMR <manuelhmrjf@gmail.com>
2022-02-07 12:56:01 -0300

    lock 3 cartas

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-let pairNum = 0;
+let num = 0;
 let myInterval;
 let tick = document.querySelector(".clock");
 let counter = document.querySelector(".moves");
+let flipped = document.querySelectorAll(".flip");
+let boardLocker = false;
 const backImg0 =
   '<img src="media/bobrossparrot.gif" alt="gif de papagaio bob ross"/'
 const backImg1 =
@@ -27,21 +29,22 @@ let backImgArr = [
 ]
 backImgArr = backImgArr.sort(() => Math.random() - 0.5)
 const main = document.querySelector('main')
+gameSize()
 function displayCards() {
   main.innerHTML = ``
   let selectedCards = []
   let j = 0
-  for (let i = 0; i < pairNum; i++) {
+  for (let i = 0; i < num/2; i++) {
     selectedCards.push(backImgArr[i])
     selectedCards.push(backImgArr[i])
   }
   selectedCards = selectedCards.sort(() => Math.random() - 0.5)
   for (j = 0; j < selectedCards.length; j++) {
-    let card = `<div class="card" onclick="flipCard(this)">
+    let card = `<div class="card" onclick="flipCard(this)" data-identifier="card">
     <div class="cardFront">
-    <img src="media/front.png" alt="papagaio verde fofinho"/>
+    <img src="media/front.png" alt="papagaio verde fofinho"/ data-identifier="front-face">
     </div>
-    <div class="cardBack">
+    <div class="cardBack" data-identifier="back-face">
     ${selectedCards[j]}
     </div>
     </div>`
@@ -52,12 +55,12 @@ function displayCards() {
   tick.innerHTML = parseInt(tick.innerHTML) + 1
  }
 function gameSize() {
-  pairNum = prompt(
-    'Com quantos pares de cartas deseja jogar? (limite 2-7 pares)'
-  )
-  while (pairNum > 7 || pairNum < 2) {
-    pairNum = parseInt(
-      prompt('Com quantos pares de cartas deseja jogar? (limite 2-7 pares)')
+  num = parseInt(prompt(
+    'Com quantas cartas deseja jogar? (4-14 cartas, pares)'
+  ))
+  while (num < 4 || num > 14 || num%2 != 0) {
+    num = parseInt(
+      prompt('Com quantos pares de cartas deseja jogar? (limite 4-14 cartas, pares)')
     )
   }
   displayCards()
@@ -67,23 +70,39 @@ function gameSize() {
   myInterval = setInterval(gameTimer, 1000)
 }
  function flipCard(e){
-  e.classList.add("flip");
   counter.innerHTML = parseInt(counter.innerHTML) + 1;
   let classCheck = e.classList.contains('selected')
+  if (boardLocker === true){
+    return;
+  }
   if(classCheck === false){
     e.classList.add("selected")
   }
   let selected = document.querySelectorAll(".selected")
   if(selected.length === 1){}
-  if(selected.length ===2){
+  if(selected.length === 2){
     if(selected[0].innerHTML === selected[1].innerHTML){document.querySelectorAll(".selected").forEach(e => e.classList.remove("selected"))}
     if(selected [0].innerHTML !== selected[1].innerHTML){
       setTimeout(function(){
         document.querySelectorAll(".selected").forEach(e => e.classList.remove("flip"))
         document.querySelectorAll(".selected").forEach(e => e.classList.remove("selected"))},
         1000)
-    } 
+    }
   }
+  if(selected.length >= 2){
+    boardLocker = true;
+    setTimeout(function(){
+      boardLocker = false
+    }, 1050)
+  }
+  e.classList.add("flip");
+  flipped = document.querySelectorAll(".flip")
+  checkFinish()
  }
+function checkFinish(){
+if(flipped.length === num && flipped.length !== 0){
+  alert(`Você ganhou em ${counter.innerHTML} jogadas!
 
- 
+  Tempo de jogo: ${tick.innerHTML} segundos`)
+  } 
+}
